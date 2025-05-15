0ae5142d310e714b54d0ebbab3ca207036c6c6f6
Ghibli <masathink@outlook.jp>
2025-05-15 18:30:33 +0900

    計算画面を編集中

--- src/main/java/com/example/demo/controller/CalculateController.java
+++ src/main/java/com/example/demo/controller/CalculateController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CalculateController {
 	@GetMapping("/calculate")
 	
-    public String showCalculate() {
+    public String execute() {
+		// 値の初期化
+		// 大分類のプルダウン値取得
+		// 小分類のプルダウン値取得
         return "calculate"; // calculate.htmlを表示
     }
 }
