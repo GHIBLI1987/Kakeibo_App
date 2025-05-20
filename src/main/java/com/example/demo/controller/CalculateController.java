5126af7b2c628debb13094856daeaad2c26bf9c9
Ghibli <masathink@outlook.jp>
2025-05-20 19:02:24 +0900

    プルダウン選択値をJavaへ渡す処理は完了。Javaから画面へ渡す処理調査中

--- src/main/java/com/example/demo/controller/CalculateController.java
+++ src/main/java/com/example/demo/controller/CalculateController.java
@@ -3,21 +3,20 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.model.MCategory;
-import com.example.demo.model.repository.CategoryMapper;
 import com.example.demo.model.service.CalculateService;
 
 @Controller
 public class CalculateController {
 	@Autowired
-	private JdbcTemplate jdbcTemplate;
-	
 	private CalculateService service;
+	private Model local_model;
 	
 	
 	@GetMapping("/calculate")
@@ -26,11 +25,19 @@ public class CalculateController {
 		// 値の初期化
 		model.addAttribute("gram", 1);
 		// 大分類のプルダウン値取得
-		List<MCategory> bigDirectionList = jdbcTemplate.queryForObject("select * from price_db.M_Category where parent_id is Null", new CategoryMapper());
+		List<MCategory> bigDirectionList = service.getBigDirectionList();
 		model.addAttribute("bigDirectionList", bigDirectionList);
 		// 小分類のプルダウン値取得
-		List<MCategory> littleDirectionList = null;
+		List<MCategory> littleDirectionList = service.getLittleDirectionList("");
 		model.addAttribute("littleDirectionList", littleDirectionList);
+		local_model=model;
+        return "calculate"; // calculate.htmlを表示
+    }
+	
+	@PostMapping("/calculate/changeBigDirection")
+    public String changeBigDirection(@ModelAttribute("littleDirectionList") String bigDirection) {
+		// 小分類のプルダウン値取得
+		List<MCategory> littleDirectionList = service.getLittleDirectionList(bigDirection);
         return "calculate"; // calculate.htmlを表示
     }
 }
--- src/main/java/com/example/demo/model/service/CalculateService.java
+++ src/main/java/com/example/demo/model/service/CalculateService.java
@@ -1,8 +1,32 @@
 package com.example.demo.model.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.model.MCategory;
+import com.example.demo.model.repository.CategoryMapper;
 
 @Service
 public class CalculateService {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public List<MCategory> getBigDirectionList() {
+		List<MCategory> bigDirectionList = jdbcTemplate.queryForObject("select * from price_db.M_Category where parent_id is Null", new CategoryMapper());
+		
+		return bigDirectionList;
+	}
+	public List<MCategory> getLittleDirectionList(String bigDirection) {
+		List<MCategory> littleDirectionList = null;
+		if(!bigDirection.isBlank()) {
+			littleDirectionList = jdbcTemplate.queryForObject("select * from price_db.M_Category where parent_id ="+bigDirection, new CategoryMapper());
+		}
+		
+		
+		return littleDirectionList;
+	}
 
 }
